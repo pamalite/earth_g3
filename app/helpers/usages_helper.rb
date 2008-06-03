d787eea6336470e2a24aa1333c65bca4bbcdaa1c
Ken S'ng Wong <ken.wong@student.adelaide.edu.au>
2008-06-03 20:58:55 +0930

    added usages_helper.rb and usages_routes.rb for primitive plugin prototype

---
+++ app/helpers/usages_helper.rb
@@ -0,0 +1,23 @@
+module UsagesHelper
+
+  $the_sections << :usages
+  
+  $tab_info << {:title => "user usages", :controller => "browser", :action => "usages"}
+      
+  def usages
+    server = Earth::Server.find_by_name(params[:server])
+    if server == nil then
+      @users_space_usages = Earth::File.find(:all, 
+                                              :select => "sum(files.bytes) as space_usage, files.uid, directories.server_id",
+                                              :joins => "join directories on files.directory_id = directories.id",
+                                              :group => "files.uid, directories.server_id")
+    else
+      @users_space_usages = Earth::File.find(:all,
+                                              :select => "sum(files.bytes) as space_usage, files.uid, directories.server_id",
+                                              :joins => "join directories on files.directory_id = directories.id",
+                                              :group => "files.uid, directories.server_id",
+                                              :conditions => [ "server_id = ? ", server ])
+    end
+  end
+end
+  
---
+++ config/usage_routes.rb
@@ -0,0 +1,12 @@
+module UsageRoutes
+
+  def mapPluginRoute(map, hostname_regex)
+  map.connect '/browser/usages', :controller => "browser", :action => "usages"
+  map.connect '/browser/usages/:server', :controller => "browser", :action => "usages",
+    :requirements => {:server => hostname_regex}
+  map.connect '/browser/usages/:server*path', :controller => "browser", :action => "usages",
+    :requirements => {:server => hostname_regex}
+  map.connect '/browser/usages.:format/:server*path', :controller => "browser", :action => "usages",
+    :requirements => {:server => hostname_regex}
+  end
+end
